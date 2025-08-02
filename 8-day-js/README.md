a81ab00092372cfe4d81d84a680384ce6a75c9a1
Sakib Fakir <FakirSakib22232@gmail.com>
2025-08-02 17:36:58 +0600

    done

---
+++ 8-day-js/README.md
@@ -0,0 +1,53 @@
+
+<!-- micro -->
+<!-- JavaScript Execution Context -->
+
+
+
+lexical scope where my code postoin 
+code phycal placed
+
+
+undefine create but never gave value 
+
+
+
+
+# Context 
+<!-- Runing this code  -->
+
+ 1. Global Execution Context -> any thing outsidde in function
+
+   1.Creation phase
+  2 . Execution -> assin value to variable 
+  
+
+
+
+  
+
+
+
+
+
+
+Window brower object
+
+this = object -> refer global object
+window = object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+LINK : https://miro.com/welcomeonboard/MlNFeTFQMkxzZ2ZocEx0ZFV5OHVZK3dTUUQ3dlpXUUUxMVYwb3A3ZFRMeUlGUDhuNTUvZ2dqNGltb0pndlVpa1QwQXh0dE5MdDlzZStzWGdGelgvNnl2anhKS0pIZjkrV3VrRDNLN1I3QVRBUlE2UVhWNSt4dVh6ZnF2eG8xT21NakdSWkpBejJWRjJhRnhhb1UwcS9BPT0hdjE=?share_link_id=720391542853
---
+++ 8-day-js/sakib.js
@@ -0,0 +1,53 @@
+// Please complete the following tasks and post them on the tapaScript Discord under "40 Days of JavaScript".
+
+// DO NOT USE AI to FIND ANSWERS. If you are stuck, let's discuss it on DISCORD and learn. Also, please note that none of the answers need you to create any UI. Just focus on the logic building and print the output on the browser console.
+
+// 1. Draw the Execution Context Diagram of the follwoing code and share as explained below:
+
+first create global Execution Unit(GEU)
+1. go message line and store as undefined
+2. than go sum function and sum functino here create functino execution under global execution unit 
+ store sum functin as and result gave undefined than go another line 
+
+ execution phase gave all value in variable and function
+
+
+
+
+
+
+// function mul(a, b) {
+//     const result = a * b;
+//     return result;
+// }
+
+1. create global execution unit than create functino execution unit mul 
+ # inside mul have
+2 phase
+1. Creation
+2.execution
+
+1.
+result: undefine
+
+
+// function calc(a, b) {
+//     return (sum(a, b) + mul(a,b))/2;
+// }
+1. Gsc create function execution unit 
+ # function create just functino name store
+
+
+// function getResult(a, b) {
+//     return calc(a, b);
+// }
+
+here just create gsc and function unit 
+
+// getResult(8, 5);
+
+
+// Create the GEC and FEC with CP and EP flow
+// Create the Stack and Heap Flow
+// Create the Stack Diagram
+// Create a Readme file with all the above diagram and share on Discord.
