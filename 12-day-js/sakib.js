6583e8873f75ddc605e91f798b21633e06b62350
Sakib Fakir <FakirSakib22232@gmail.com>
2025-09-13 12:59:57 +0600

    3 probelm solved

--- 12-day-js/sakib.js
+++ 12-day-js/sakib.js
@@ -1,3 +1,100 @@
 
+// # Tasks
 
-let a = 10;
+// Please complete the following tasks and post them on the tapaScript Discord under "40 Days of JavaScript".
+
+// > **DO NOT USE AI to FIND ANSWERS**. If you are stuck, let's discuss it on DISCORD and learn. Also, please note that none of the answers need you to create any UI. Just focus on the logic building and print the output on the browser console.
+
+// ## 1. What will be the output and why?
+
+// ```js
+// const user = { name: "Alex", age: undefined };
+// console.log(user.age ?? "Not provided");
+// ```
+
+// output -> not provide because here nullish colision oprator chck null , undefine return right side 
+
+// ## 2. What will happen if we try to modify a frozen object?
+
+// ```js
+// const obj = Object.freeze({ a: 1 });
+// obj.a = 2;
+// console.log(obj.a);
+// ```
+
+/// output -> 1 because frezze do not allow to change object value 
+
+
+
+
+
+// ## 3. Given an object with deeply nested properties, extract name, company, and address.city using destructuring
+
+// ```js
+const person = {
+  name: "Tapas",
+  company: {
+    name: "tapaScript",
+    location: {
+      city: "Bangalore",
+      zip: "94107"
+    }
+  }
+};
+
+
+ const {name, company ,  } = person;
+ console.log(name, company , company.location.city)
+
+
+
+// ## 4. Build a Student Management System
+
+// - Store student details in an object (name, age, grades).
+// - Implement a method to calculate the average grade.
+
+// ## 5. Book Store Inventory System
+
+// - Store books in an object.
+// - Add functionality to check availability and restock books.
+
+// ## 6. What is the difference between Object.keys() and Object.entries()? Explain with examples
+
+// ## 7. How do you check if an object has a certain property?
+
+// ## 8. What will be the output and why?
+
+// ```js
+// const person = { name: "John" };
+// const newPerson = person;
+// newPerson.name = "Doe";
+// console.log(person.name);
+// ```
+
+// ## 9. What’s the best way to deeply copy a nested object? Expalin with examples
+
+// ## 10. Loop and print values using Object destructuiring
+
+// ```js
+const users = [
+  {
+      'name': 'Alex',
+      'address': '15th Park Avenue',
+      'age': 43
+  },
+  {
+      'name': 'Bob',
+      'address': 'Canada',
+      'age': 53
+  },
+  {
+      'name': 'Carl',
+      'address': 'Bangalore',
+      'age': 26
+  }
+];
+
+
+users.forEach((item,key)=>{
+    const {} = item;
+})
